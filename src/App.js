4103fe88c09fc1f37098c704a9e16f273f2b8870
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-24 11:23:16 +0100

    Add SearchBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ProductList from './containers/product-list/product-list';
 import ProductDetail from './containers/product-detail/product-detail';
+import SearchBar from './components/search-bar/search-bar';
 
 function App() {
   return (
     <div className="App">
       <h1>Demo Redux + Ajax</h1>
+      <SearchBar />
       <ProductList />
       <ProductDetail />
     </div>
---
+++ src/components/search-bar/search-bar.jsx
@@ -0,0 +1,38 @@
+import { useForm } from 'react-hook-form';
+import PropTypes from 'prop-types';
+
+const SearchBar = (props) => {
+    const {submitName} = props;
+    const { register, handleSubmit, reset , formState: { errors } } = useForm();
+    /* Brieve description des elements du "useForm"
+        - handleSubmit: Gere le preventDefault et permet d'obtenir les datas
+        - register: Permet de "lier" les balises html et le state de react
+        - reset: Permet de déclancher un "reset" du formulaire
+        - formState: Permet d'obtenir des infos sur l'etat du form (Les erreurs, par exemple)
+    */
+
+    const handleSearch = (data) => {
+        props.onResult(data.content);
+        reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleSearch)}>
+            <input type='text' {...register("content", { required: true })}  />
+            <button type='submit'>{submitName}</button>
+            { errors.content && <div>Je cherche pas du vent moi! Tape du text! :o</div>} 
+        </form>
+    )
+}
+
+SearchBar.defaultProps = { 
+    submitName: 'Rechercher',
+    onResult: () => {}          // NOOP
+}
+
+SearchBar.propTypes = { 
+    submitName: PropTypes.string,
+    onResult: PropTypes.func 
+}
+
+export default SearchBar;
